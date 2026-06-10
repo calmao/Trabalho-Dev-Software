92747dd1eee2161b77946e2dd2039212612df9d2
danilo-vm <d4nilo1887@gmail.com>
2026-06-10 00:03:56 -0300

    arquivos desnecessarios removidos

--- Interface-de-usuário/novofile.py
+++
@@ -1,4 +0,0 @@
-import classe_interface 
-
-interface = classe_interface.InterfaceGrafica()
-interface.mainloop()
---
+++ testar_interface_classe.py
@@ -0,0 +1,3 @@
+import classe_interface as gui
+interface = gui.InterfaceGrafica()
+interface.mainloop()
