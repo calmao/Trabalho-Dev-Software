b330fb6d17c263ccffa15861834940e400cd28ad
pedro <pedro.strauch.fleury@gmail.com>
2026-06-11 13:09:34 -0300

    resolvi um bug de leitura

--- main.py
+++ main.py
@@ -7,16 +7,10 @@ import classe_interface as ui
 
     
 def main():
-<<<<<<< HEAD
-    vol = [100, 100]
-    oitava = [4, 3]
-    inst = [0, 34, 117]
-=======
     '''
     vol = [100,100]
     oitava = [4,3]
     inst = [127,127]
->>>>>>> 4940652244e293a916f5ba9bbfe8d69b9307ff1d
     bpm = 60
 
     
@@ -29,17 +23,11 @@ def main():
     m.iniciar(p)
     #print(m.mid.length)
     #print(m.mid.tracks)
-<<<<<<< HEAD
-    m.tocar()
-    #m.tocar()
-    #m.salvar('doremifa.mid')
-=======
     #m.tocar()
     m.salvar('doremifa.mid')'''
 
     interface = ui.InterfaceGrafica()
     interface.mainloop()
->>>>>>> 4940652244e293a916f5ba9bbfe8d69b9307ff1d
 
     return
 main()
